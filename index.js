ad23b3a453b99f4e461571fa633255e1a502df24
Mayck <103364944+MayckL2@users.noreply.github.com>
2023-11-03 01:08:33 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const User = require('./models/User')
 const app = express()
 
 require('dotenv').config()
@@ -10,85 +10,86 @@ const connectDB = require('./connectMongo')
 
 connectDB()
 
-const BookModel = require('./models/book.model')
+// const BookModel = require('./models/book.model')
 
 app.get('/api/', async (req, res) => {
-    res.send({msg: 'tudo certo!'})
+
+    res.send(await User.find())
 })
 
-app.get('/api/v1/books/:id', async (req, res) => {
-    try {
-        const data = await BookModel.findById(req.params.id)
+// app.get('/api/v1/books/:id', async (req, res) => {
+//     try {
+//         const data = await BookModel.findById(req.params.id)
 
-        if (data) {
-            return res.status(200).json({
-                msg: 'Ok',
-                data
-            })
-        }
+//         if (data) {
+//             return res.status(200).json({
+//                 msg: 'Ok',
+//                 data
+//             })
+//         }
 
-        return res.status(404).json({
-            msg: 'Not Found',
-        })
-    } catch (error) {
-        return res.status(500).json({
-            msg: error.message
-        })
-    }
-})
+//         return res.status(404).json({
+//             msg: 'Not Found',
+//         })
+//     } catch (error) {
+//         return res.status(500).json({
+//             msg: error.message
+//         })
+//     }
+// })
 
-app.post('/api/v1/books', async (req, res) => {
-    try {
-        const { name, author, price, description } = req.body
-        const book = new BookModel({
-            name, author, price, description
-        })
-        const data = await book.save()
-        return res.status(200).json({
-            msg: 'Ok',
-            data
-        })
-    } catch (error) {
-        return res.status(500).json({
-            msg: error.message
-        })
-    }
-})
+// app.post('/api/v1/books', async (req, res) => {
+//     try {
+//         const { name, author, price, description } = req.body
+//         const book = new BookModel({
+//             name, author, price, description
+//         })
+//         const data = await book.save()
+//         return res.status(200).json({
+//             msg: 'Ok',
+//             data
+//         })
+//     } catch (error) {
+//         return res.status(500).json({
+//             msg: error.message
+//         })
+//     }
+// })
 
-app.put('/api/v1/books/:id', async (req, res) => {
-    try {
-        const { name, author, price, description } = req.body
-        const { id } = req.params
+// app.put('/api/v1/books/:id', async (req, res) => {
+//     try {
+//         const { name, author, price, description } = req.body
+//         const { id } = req.params
 
-        const data = await BookModel.findByIdAndUpdate(id, {
-            name, author, price, description
-        }, { new: true })
+//         const data = await BookModel.findByIdAndUpdate(id, {
+//             name, author, price, description
+//         }, { new: true })
 
-        return res.status(200).json({
-            msg: 'Ok',
-            data
-        })
-    } catch (error) {
-        return res.status(500).json({
-            msg: error.message
-        })
-    }
-})
+//         return res.status(200).json({
+//             msg: 'Ok',
+//             data
+//         })
+//     } catch (error) {
+//         return res.status(500).json({
+//             msg: error.message
+//         })
+//     }
+// })
 
-app.delete('/api/v1/books/:id', async (req, res) => {
-    try {
-        await BookModel.findByIdAndDelete(req.params.id)
-        return res.status(200).json({
-            msg: 'Ok',
-        })
-    } catch (error) {
-        return res.status(500).json({
-            msg: error.message
-        })
-    }
-})
+// app.delete('/api/v1/books/:id', async (req, res) => {
+//     try {
+//         await BookModel.findByIdAndDelete(req.params.id)
+//         return res.status(200).json({
+//             msg: 'Ok',
+//         })
+//     } catch (error) {
+//         return res.status(500).json({
+//             msg: error.message
+//         })
+//     }
+// })
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
     console.log("Server is running on port " + PORT)
